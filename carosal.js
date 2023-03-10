65f0bfc24dc9a3b0634f25c5ef4d0c84ccce3378
avez07 <ansariaavez12345@gmail.com>
2023-03-10 17:22:44 +0530

    ovl carosal updated

---
+++ carosal.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:["<span class='arrow'><i class='fa-solid fa-circle-chevron-left'></i><span>","<span class='arrow'><i class='fa-solid fa-circle-chevron-right'></i><span>"],
+    responsive:{
+        0:{
+            items:4,
+            nav:false,
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+})
